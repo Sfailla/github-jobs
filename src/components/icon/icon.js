5820de62fd379669369e765fccdf52e010873aa4
Sfailla <Sfailla1983@gmail.com>
2021-01-25 11:59:54 -0500

    added Icon component and styles

---
+++ src/components/icon/icon.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Image } from './iconStyles';
+
+export default function Icon({ src, alt }) {
+  return <Image src={src} alt={alt} />;
+}
---
+++ src/components/icon/iconStyles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/macro';
+
+export const Image = styled.img`
+  width: 2.4rem;
+  height: 2.4rem;
+`;
