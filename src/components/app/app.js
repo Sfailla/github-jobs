68a79bcfe0dbd3a243cbadd402c5c82fc3ee4960
Sfailla <Sfailla1983@gmail.com>
2021-01-29 22:20:53 -0500

    added logic for theming - light and dark mode

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from '../header';
+import Searchbar from '../searchbar';
+import Page from '../page';
+
+import { ThemeProvider } from 'styled-components';
 
 function App() {
   return (
-    <React.Fragment>
-      <Header />
-    </React.Fragment>
+    <ThemeProvider theme={{ mode: 'light' }}>
+      <React.Fragment>
+        <Header>
+          <Searchbar />
+        </Header>
+        <Page name="jobs" />
+      </React.Fragment>
+    </ThemeProvider>
   );
 }
 
