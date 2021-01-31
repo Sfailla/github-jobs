acf5c2472a79e13108a11f9358fbc6fdfd3301e7
Sfailla <Sfailla1983@gmail.com>
2021-01-31 10:45:16 -0500

    added theme context for light and dark mode

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,19 +2,23 @@ import React from 'react';
 import Header from '../header';
 import Searchbar from '../searchbar';
 import Page from '../page';
+import { ThemeContext } from './context';
 
 import { ThemeProvider } from 'styled-components';
 
 function App() {
+  const [theme, setTheme] = React.useState({ mode: 'light' });
   return (
-    <ThemeProvider theme={{ mode: 'light' }}>
-      <React.Fragment>
-        <Header>
-          <Searchbar />
-        </Header>
-        <Page name="jobs" />
-      </React.Fragment>
-    </ThemeProvider>
+    <ThemeContext.Provider value={{ setTheme }}>
+      <ThemeProvider theme={theme}>
+        <React.Fragment>
+          <Header>
+            <Searchbar />
+          </Header>
+          <Page name="jobs" />
+        </React.Fragment>
+      </ThemeProvider>
+    </ThemeContext.Provider>
   );
 }
 
