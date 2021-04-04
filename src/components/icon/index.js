9e88f3f327cb921b5e777706572015baae01fe42
Sfailla <Sfailla1983@gmail.com>
2021-04-04 10:30:24 -0400

    added icon component and styles

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image } from './iconStyles';
 
-export default function Icon({ ...otherProps }) {
-  return <Image {...otherProps} />;
+export default function Icon({ width = 24, height = 24, ...otherProps }) {
+  return <Image width={width} height={height} {...otherProps} />;
 }
