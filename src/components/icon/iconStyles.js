27ec5584235d864a4399d6a8b84f62619f86b8f2
Sfailla <Sfailla1983@gmail.com>
2021-04-16 13:45:47 -0400

    removed icon component. will use react svg components instead

--- src/components/icon/iconStyles.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components/macro';
-
-export const Image = styled.img`
-  ${({ width, height }) => `width: ${width}px; height: ${height}px`};
-`;
--- src/components/icon/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Image } from './iconStyles';
-
-export default function Icon({ width = 24, height = 24, ...otherProps }) {
-  return <Image width={width} height={height} {...otherProps} />;
-}
