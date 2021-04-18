ed494faf1e2281f8e57537e6e1a05a2e0132813d
Sfailla <Sfailla1983@gmail.com>
2021-04-18 14:31:45 -0400

    fixed import path for theme

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import { theme } from '../styles/theme'
+import { theme } from '../styles/theme.js'
 
 export const AppContext = React.createContext()
 
