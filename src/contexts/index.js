d4c99623d9b9e12460fdb37916394257c331db51
Sfailla <Sfailla1983@gmail.com>
2021-04-18 13:05:35 -0400

    formatted file

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { Theme } from '../styles/Theme';
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../styles/Theme'
 
-export const AppContext = React.createContext();
+export const AppContext = React.createContext()
 
 export const AppContextProvider = ({ children }) => {
-  const [showModal, setShowModal] = React.useState(false);
-  const [mode, setMode] = React.useState('light');
+  const [showModal, setShowModal] = React.useState(false)
+  const [mode, setMode] = React.useState('light')
 
   const values = {
     showModal,
     setShowModal,
     setMode
-  };
+  }
 
   return (
     <AppContext.Provider value={values}>
-      <ThemeProvider theme={{ mode, theme: Theme }}>{children}</ThemeProvider>
+      <ThemeProvider theme={{ theme, mode }}>{children}</ThemeProvider>
     </AppContext.Provider>
-  );
-};
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/app';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app/app'
 // import reportWebVitals from './reportWebVitals';
-import { GlobalStyles } from './styles/global';
+import { GlobalStyles } from './styles/global'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,7 +10,7 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('⚛️')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
