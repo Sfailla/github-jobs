9660727958c6d2c52fd186c8913bda8d64055c54
Sfailla <Sfailla1983@gmail.com>
2021-04-28 12:42:31 -0400

    updated wrapper for tests by adding AppContext which provides all wrappers

--- src/test/test-utils.js
+++ src/test/test-utils.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { ThemeProvider } from 'styled-components'
-import { theme } from '../styles/theme'
+import { AppContextProvider } from '../contexts'
 
-function renderWithTheme(component, { lightTheme = 'light', ...options } = {}) {
-  const Wrapper = ({ children }) => (
-    <ThemeProvider theme={theme[lightTheme]}>{children}</ThemeProvider>
-  )
+function renderWithTheme(component, { ...options } = {}) {
+  const Wrapper = ({ children }) => <AppContextProvider>{children}</AppContextProvider>
 
   return render(component, { wrapper: Wrapper, ...options })
 }
