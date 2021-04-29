1d4c737fcfa513c65b8df6abc0c5ec05badb2ae3
Sfailla <Sfailla1983@gmail.com>
2021-04-29 14:42:07 -0400

    added new stucture where I will render 2 different components for searchbar.. desktop and mobile

---
+++ src/components/searchbar/desktop/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Searchbar from '..'
+import { SearchIcon, LocationIcon } from '../../../icons'
+import { Wrapper, SvgWrapper } from './style'
+import { SearchbarButton as Button } from '../../button'
+import { useWindowSize } from '../../../hooks'
+
+const SearchbarContainer = () => {
+  const [checked, setChecked] = React.useState(false)
+  const handleCheck = () => setChecked(checked => !checked)
+  const { width } = useWindowSize()
+  const shortenText = width < 992
+
+  return (
+    <Searchbar>
+      <Searchbar.Section>
+        <Searchbar.InputGroup>
+          <Searchbar.Input placeholder=" " />
+          <Wrapper>
+            <SearchIcon />
+            <Searchbar.Label>
+              {shortenText
+                ? 'Filter by title...'
+                : 'Filter by title, companies, expertise...'}
+            </Searchbar.Label>
+          </Wrapper>
+        </Searchbar.InputGroup>
+      </Searchbar.Section>
+
+      <Searchbar.Section>
+        <Searchbar.InputGroup>
+          <Searchbar.Input placeholder=" " />
+          <Wrapper>
+            <SvgWrapper>
+              <LocationIcon />
+            </SvgWrapper>
+            <Searchbar.Label>Filter by location...</Searchbar.Label>
+          </Wrapper>
+        </Searchbar.InputGroup>
+      </Searchbar.Section>
+
+      <Searchbar.Section>
+        <Searchbar.InputGroup>
+          <Wrapper>
+            <Searchbar.Checkbox onChange={handleCheck} checked={checked} />
+            <Searchbar.Label style={{ marginRight: 'auto' }} bold>
+              Find a job
+            </Searchbar.Label>
+            <Button>Search</Button>
+          </Wrapper>
+        </Searchbar.InputGroup>
+      </Searchbar.Section>
+    </Searchbar>
+  )
+}
+
+export default SearchbarContainer
