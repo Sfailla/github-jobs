c3c175e9bee9ffb9351ca1918c6755178b628d76
Sfailla <Sfailla1983@gmail.com>
2021-05-15 09:55:26 -0400

    added random colors helper fn

--- src/utils/helperFns.js
+++ src/utils/helperFns.js
@@ -5,3 +5,13 @@ export const formatTime = time => moment(Date.parse(time)).fromNow()
 export const truncateWord = string => {
   return string?.length > 25 ? `${string.substring(0, 25)}...` : string
 }
+
+export const handleNullLogo = (logo, svg) => {
+  return !logo ? svg : logo
+}
+
+const colors = ['#DF6DAE', '#3DB3D1', '#3D3B94', '#F0B62A', '#60DCAD', '#FF585F']
+
+export const randomColor = () => {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
