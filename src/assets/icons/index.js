858d677826eaf082ebcf13278fa5f55fbfc14200
Sfailla <Sfailla1983@gmail.com>
2021-05-15 09:29:19 -0400

    added main import for icon components

---
+++ src/assets/icons/index.js
@@ -0,0 +1,7 @@
+import { ReactComponent as SearchIcon } from '../desktop/icon-search.svg'
+import { ReactComponent as LocationIcon } from '../desktop/icon-location.svg'
+import { ReactComponent as CheckmarkIcon } from '../desktop/icon-check.svg'
+import { ReactComponent as SunIcon } from '../desktop/icon-sun.svg'
+import { ReactComponent as MoonIcon } from '../desktop/icon-moon.svg'
+
+export { SearchIcon, LocationIcon, CheckmarkIcon, SunIcon, MoonIcon }
