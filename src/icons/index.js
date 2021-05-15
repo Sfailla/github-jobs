7286bd23f2cc563c8369af4b01beb3cf834b4c8a
Sfailla <Sfailla1983@gmail.com>
2021-05-15 10:06:26 -0400

    removed file

--- src/icons/index.js
+++
@@ -1,7 +0,0 @@
-import { ReactComponent as SearchIcon } from '../assets/desktop/icon-search.svg'
-import { ReactComponent as LocationIcon } from '../assets/desktop/icon-location.svg'
-import { ReactComponent as CheckmarkIcon } from '../assets/desktop/icon-check.svg'
-import { ReactComponent as SunIcon } from '../assets/desktop/icon-sun.svg'
-import { ReactComponent as MoonIcon } from '../assets/desktop/icon-moon.svg'
-
-export { SearchIcon, LocationIcon, CheckmarkIcon, SunIcon, MoonIcon }
