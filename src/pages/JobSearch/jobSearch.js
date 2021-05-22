d7e7544457af16960e05260ef4d4a4539fda45c3
Sfailla <Sfailla1983@gmail.com>
2021-05-22 15:03:17 -0400

    continued implementation of search and filter functionality in job search component

--- src/pages/JobSearch/jobSearch.js
+++ src/pages/JobSearch/jobSearch.js
@@ -5,45 +5,55 @@ import { InfoCard as Card } from '../../components/cards'
 import Searchbar from '../../components/searchbar/desktop'
 import { LayoutWrapper } from '../../styles/shared'
 
+const INITIAL_VALUES = {
+  search: null,
+  location: null,
+  full_time: false
+}
+
 function JobSearch() {
   const [checked, setChecked] = React.useState(false)
+  const [{ search, location, full_time }, setValues] = React.useState(INITIAL_VALUES)
+  const [updateQuery, setUpdateQuery] = React.useState({})
+
   const handleCheck = React.useCallback(() => setChecked(checked => !checked), [])
 
-  const INITIAL_VALUES = {
-    search: '',
-    location: '',
-    fullTime: ''
-  }
-
-  const [values, setValues] = React.useState(INITIAL_VALUES)
-
   function handleChange(event) {
+    const { name, value } = event.target
     event.persist()
 
-    setValues({
-      [event.target.name]: event.target.value
-    })
+    setValues({ [name]: value })
   }
 
   function handleSubmit(event) {
     event.preventDefault()
-    console.log(values)
+    setUpdateQuery({ search, location, full_time, page: 1 })
   }
 
-  const query = useBuildQuery()
+  const query = useBuildQuery({ ...updateQuery })
+
+  console.log(query)
+
   const { results, isLoading } = useFetchData(query)
 
   return (
     <Container>
       <LayoutWrapper>
         <Searchbar {...{ checked, handleCheck, handleChange, handleSubmit }} />
-        <GridContainer>
-          {isLoading ? (
-            <div>please wait while we load your data...</div>
-          ) : (
-            results.length > 0 && results.map(data => <Card key={data.id} data={data} />)
-          )}
-        </GridContainer>
+
+        {isLoading && (
+          <div style={{ paddingTop: '8rem' }}>please wait while we load your data...</div>
+        )}
+
+        {!isLoading && results.length < 1 ? (
+          <div style={{ paddingTop: '8rem' }}>
+            Sorry no results were found 😕. Please try searching another job!
+          </div>
+        ) : (
+          <GridContainer>
+            {results.length > 0 && results.map(data => <Card key={data.id} data={data} />)}
+          </GridContainer>
+        )}
       </LayoutWrapper>
     </Container>
   )
