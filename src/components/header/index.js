e5064143780cdd571b3604d390951426098e2579
Sfailla <Sfailla1983@gmail.com>
2021-10-27 23:31:49 -0400

    simplified toggle functionality and stylnnggs

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,7 @@ import { SunIcon, MoonIcon } from '../../assets/icons'
 import { Background } from './style'
 import { LayoutWrapper } from '../../styles/shared'
 import { HeaderContent } from './style'
-import { useWindowSize, useLayoutType } from '../../hooks'
+import { useLayoutType } from '../../hooks'
 
 import { ReactComponent as Logo } from '../../assets/desktop/logo.svg'
 import desktop from '../../assets/desktop/bg-pattern-header.svg'
@@ -13,8 +13,7 @@ import tablet from '../../assets/tablet/bg-pattern-header.svg'
 import mobile from '../../assets/mobile/bg-pattern-header.svg'
 
 export default function Header() {
-  const { width } = useWindowSize()
-  const { layout } = useLayoutType(width)
+  const { layout } = useLayoutType()
 
   const handleSvgBackgroundSrc = React.useMemo(() => {
     const sources = { desktop, tablet, mobile }
@@ -28,15 +27,11 @@ export default function Header() {
           <Logo width={115} height={32} alt="logo" />
           <Toggle>
             <Toggle.Label>
-              {({ on }) => (
-                <SunIcon style={{ color: on ? 'white' : 'gold' }} width={20} height={18} />
-              )}
+              {({ on }) => <SunIcon fill={on ? 'white' : 'gold'} width={20} height={19} />}
             </Toggle.Label>
             <Toggle.Button />
             <Toggle.Label>
-              {({ on }) => (
-                <MoonIcon style={{ color: on ? 'gold' : 'white' }} width={12} height={12} />
-              )}
+              {({ on }) => <MoonIcon fill={on ? 'gold' : 'white'} width={13} height={13} />}
             </Toggle.Label>
           </Toggle>
         </HeaderContent>
