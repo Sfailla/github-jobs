38d1a9187d1ecfa0a4d0364fee8e8aa0f00acfcf
Sfailla <Sfailla1983@gmail.com>
2021-10-31 11:03:55 -0400

    added mock folder for tests.  refactoring tests for searchbar

--- src/components/searchbar/desktop/index.spec.js
+++ src/components/searchbar/desktop/index.spec.js
@@ -1,33 +1,40 @@
 import { render, fireEvent } from '../../../test/test-utils'
+import userEvent from '@testing-library/user-event'
 
 import DesktopSearchbar from './index'
 import { SearchbarComponents as Searchbar } from '../components'
+
+const buildUserForm = () => {
+  return {
+    search: 'javascript',
+    location: 'New York'
+  }
+}
 
 describe('inputs should work correctly for desktop searchbar component', () => {
   test('search input should start with empty string and change value after onChange event', () => {
     const handleChange = jest.fn()
     const { getByLabelText } = render(<DesktopSearchbar handleChange={handleChange} />)
-    const searchInput = getByLabelText('Filter by title, companies, expertise...')
+    const searchInput = getByLabelText(/Filter by title/i)
 
-    expect(searchInput.value).toEqual('')
+    const { search } = buildUserForm()
 
-    fireEvent.change(searchInput, { target: { value: 'javascript' } })
+    userEvent.type(searchInput, search)
 
     expect(handleChange).toHaveBeenCalled()
-    expect(searchInput.value).toEqual('javascript')
+    expect(searchInput.value).toEqual(search)
   })
 
   test('location input should start with empty string and change value after onChange event', () => {
     const handleChange = jest.fn()
     const { getByLabelText } = render(<DesktopSearchbar handleChange={handleChange} />)
-    const locationInput = getByLabelText('Filter by location...')
+    const locationInput = getByLabelText(/Filter by location/i)
+    const location = 'New York'
 
-    expect(locationInput.value).toEqual('')
-
-    fireEvent.change(locationInput, { target: { value: 'New York' } })
+    userEvent.type(locationInput, location)
 
     expect(handleChange).toHaveBeenCalled()
-    expect(locationInput.value).toEqual('New York')
+    expect(locationInput.value).toEqual(location)
   })
 })
 
@@ -40,31 +47,37 @@ describe('desktop checkbox and button should work correctly', () => {
     expect(checkbox.checked).toBeFalsy()
     expect(checkbox).not.toBeChecked()
 
-    fireEvent.click(checkbox)
+    userEvent.click(checkbox)
 
     expect(handleCheck).toHaveBeenCalledTimes(1)
     expect(checkbox).toBeChecked()
     expect(checkbox.checked).toBeTruthy()
   })
 
-  test('submit button should be in the document', () => {
-    const { getByText } = render(<DesktopSearchbar />)
-    const button = getByText('Search')
-
-    expect(button).toBeInTheDocument()
-  })
-
   test('submit button should trigger onSubmit function in parent component', () => {
-    const handleSubmit = jest.fn(e => e.preventDefault())
-    const { getByText } = render(
+    const handleSubmit = jest.fn()
+    const { getByRole, getByLabelText } = render(
       <Searchbar onSubmit={handleSubmit}>
         <DesktopSearchbar />
       </Searchbar>
     )
-    const button = getByText('Search')
-    fireEvent.click(button)
+
+    const searchInput = getByLabelText(/Filter by title/i)
+    const locationInput = getByLabelText(/Filter by location/i)
+    const button = getByRole('button', { name: /search/i })
+
+    const { search, location } = buildUserForm()
+
+    userEvent.type(searchInput, search)
+    userEvent.type(locationInput, location)
+
+    userEvent.click(button)
 
     expect(button).toBeInTheDocument()
     expect(handleSubmit).toHaveBeenCalledTimes(1)
+    expect(handleSubmit).toHaveBeenCalledWith({
+      search,
+      location
+    })
   })
 })
