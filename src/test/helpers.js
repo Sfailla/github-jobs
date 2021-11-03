04b94158af5dc9af3a78c5c8358adc1cfe255bb4
Sfailla <Sfailla1983@gmail.com>
2021-11-03 09:22:14 -0400

    added stubbedJobs to test/helpers file

--- src/test/helpers.js
+++ src/test/helpers.js
@@ -4,3 +4,20 @@ export const buildSearchForm = () => {
     location: 'New York'
   }
 }
+
+export const stubbedJobs = {
+  results: [
+    {
+      id: '5514a0a7',
+      company: 'Canada Life',
+      title: 'Software Developer',
+      location: 'Toronto, Ontario, Canada'
+    },
+    {
+      id: '7fe0-4647',
+      company: 'Enterprise',
+      title: 'Junior Software Developer',
+      location: 'New York City, New York'
+    }
+  ]
+}
