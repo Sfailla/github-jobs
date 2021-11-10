66c4ae029baedf155f074f18b1657d4e0b840a76
Sfailla <Sfailla1983@gmail.com>
2021-11-09 23:09:18 -0500

    updated test case

--- src/test/test-utils.js
+++ src/test/test-utils.js
@@ -5,11 +5,9 @@ import { AppContextProvider } from '../contexts'
 
 function renderWithTheme(component, { ...options } = {}) {
   const Wrapper = ({ children }) => <AppContextProvider>{children}</AppContextProvider>
-
   return render(component, { wrapper: Wrapper, ...options })
 }
 
 export * from '@testing-library/react'
-
 // override React Testing Library
 export { renderWithTheme as render, renderHook }
