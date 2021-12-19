a32ac8b85d8840ecd72ae25cc048c7e2da95b5a2
Sfailla <Sfailla1983@gmail.com>
2021-12-19 09:37:31 -0500

    WIP: deployment config

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,7 +1,46 @@
 import React from 'react'
 import { getColorSchemePreference } from '../utils/helperFns'
 import { ThemeProvider } from 'styled-components'
-import { theme } from '../styles/theme.js'
+// import { theme } from '../styles/theme.js'
+
+export const theme = {
+  light: {
+    background: {
+      primary: '#ECECEC',
+      secondary: '#FFFFFF'
+    },
+    input: {
+      text: '#19202D',
+      checkbox: ' rgb(25, 32, 45, .1)'
+    },
+    button: {
+      normal: 'rgba(89, 100, 224, .10)',
+      hover: 'rgba(89, 100, 224, .35)',
+      text: '#5964E0'
+    },
+    text: {
+      normal: '#19202D'
+    }
+  },
+  dark: {
+    background: {
+      primary: '#121721',
+      secondary: '#19202D'
+    },
+    input: {
+      text: '#FFFFFF',
+      checkbox: 'rgba(255, 255, 255, .10)'
+    },
+    button: {
+      normal: 'rgba(255, 255, 255, .10)',
+      hover: 'rgba(255, 255, 255, .35)',
+      text: '#FFFFFF'
+    },
+    text: {
+      normal: '#FFFFFF'
+    }
+  }
+}
 
 const AppContext = React.createContext()
 
